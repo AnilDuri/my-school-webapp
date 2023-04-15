1bd98c5951dcc8eaf64d2997c7de0ed99f085e91
Anil Duri <anilduri.a@gmail.com>
2023-04-15 23:00:37 +0300

    adding state to selected tabs on dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DashboardSectionV2 from "../components/DashboardSectionV2";
-import DashboardSection from "./../components/DashboardSection";
-import Meta from "./../components/Meta";
 import { requireAuth } from "./../util/auth";
 
 function DashboardPage(props) {
+  const [selectedTab, setSelectedTab] = useState();
   return (
     <div className="flex flex-1">
-      <DashboardSectionV2 />
+      <DashboardSectionV2 setSelectedTab={setSelectedTab} selectedTab={selectedTab} />
       <div className="flex w-5/6 justify-center items-center">
         <p>All Items will be displayed here</p>
       </div>
