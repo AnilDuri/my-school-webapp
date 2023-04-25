93476a856e54b0bab708e18ba64cc09aab4e2cb4
Anil Duri <anilduri.a@gmail.com>
2023-04-25 20:25:06 +0300

    added import button

--- src/components/parentsTab/ParentsTab.js
+++ src/components/parentsTab/ParentsTab.js
@@ -35,7 +35,14 @@ export default function ParentsTab() {
                             A list of all the Parents in your account including their name, phone number and email.
                         </p>
                     </div>
-                    <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <div className="flex gap-2">
+                        <button
+                            onClick={() => setOpen(false)}
+                            type="button"
+                            className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-indigo-600 hover:bg-gray-100"
+                        >
+                            Import
+                        </button>
                         <button
                             type="button"
                             onClick={() => setOpen(true)}
