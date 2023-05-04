d30de27157460227de7fd5925e199d7811363edd
Anil Duri <anilduri.a@gmail.com>
2023-05-04 23:47:20 +0300

    fix admin modal actions

--- src/components/adminTab/AddAdminModal.js
+++ src/components/adminTab/AddAdminModal.js
@@ -1,3 +1,4 @@
+import { Button } from '../Button'
 import Modal from '../Modal/Modal'
 
 export default function AddAdminModal({ open, setOpen }) {
@@ -6,86 +7,104 @@ export default function AddAdminModal({ open, setOpen }) {
             open={open}
             setOpen={setOpen}
             header={'Add Admin'}
-            body={<AdminModalForm />}
+            body={<AdminModalForm setOpen={setOpen} />}
         />
     )
 }
 
-const AdminModalForm = () => {
+const AdminModalForm = ({ setOpen }) => {
     return (
-        <form className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-                <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                    First name
-                </label>
-                <div className="mt-2">
-                    <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
+        <form className='flex flex-col gap-2'>
+            <div className="grid w-full grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                        First name
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            name="first-name"
+                            id="first-name"
+                            autoComplete="given-name"
+                            className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                    <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                        Last name
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            name="last-name"
+                            id="last-name"
+                            autoComplete="family-name"
+                            className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                        Email address
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                    <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
+                        Phone Number
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            name="phone-number"
+                            id="phone-number"
+                            className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+
+                <div className="sm:col-span-2">
+                    <label htmlFor="secondary-phone-number" className="block text-sm font-medium leading-6 text-gray-900">
+                        Secondary Phone Number
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            name="secondary-phone-number"
+                            id="secondary-phone-number"
+                            className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
                 </div>
             </div>
-
-            <div className="sm:col-span-3">
-                <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Last name
-                </label>
-                <div className="mt-2">
-                    <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
-                        className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-            </div>
-
-            <div className="sm:col-span-2">
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                    Email address
-                </label>
-                <div className="mt-2">
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-            </div>
-
-            <div className="sm:col-span-2">
-                <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
-                    Phone Number
-                </label>
-                <div className="mt-2">
-                    <input
-                        type="text"
-                        name="phone-number"
-                        id="phone-number"
-                        className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
-            </div>
-
-
-            <div className="sm:col-span-2">
-                <label htmlFor="secondary-phone-number" className="block text-sm font-medium leading-6 text-gray-900">
-                    Secondary Phone Number
-                </label>
-                <div className="mt-2">
-                    <input
-                        type="text"
-                        name="secondary-phone-number"
-                        id="secondary-phone-number"
-                        className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                </div>
+            <div className="flex items-center justify-end mt-4 gap-2">
+                <Button
+                    onClick={() => setOpen(false)}
+                    type="button"
+                    variant="secondary"
+                >
+                    Close
+                </Button>
+                <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={() => setOpen(false)}
+                >
+                    Save
+                </Button>
             </div>
         </form>
     )
