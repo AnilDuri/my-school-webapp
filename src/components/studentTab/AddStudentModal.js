de8a6298d509dfbe2462fb0e064ec17f6f1f58c0
Anil Duri <anilduri.a@gmail.com>
2023-05-05 00:12:05 +0300

    save new id

--- src/components/studentTab/AddStudentModal.js
+++ src/components/studentTab/AddStudentModal.js
@@ -17,14 +17,14 @@ const StudentModalForm = ({ setOpen }) => {
         <form className='flex flex-col gap-2'>
             <div className="grid w-full grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-3">
-                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label htmlFor="first_name" className="block text-sm font-medium leading-6 text-gray-900">
                         First name
                     </label>
                     <div className="mt-2">
                         <input
                             type="text"
-                            name="first-name"
-                            id="first-name"
+                            name="first_name"
+                            id="first_name"
                             autoComplete="given-name"
                             className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
